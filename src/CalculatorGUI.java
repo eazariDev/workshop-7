22eb58d58fdba78979f00f25c583d5ac6137d88f
NafrerfaN <56217942+nafre002@users.noreply.github.com>
2020-04-28 19:42:21 +0430

    GUI Calculator

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -9,22 +9,49 @@ public class CalculatorGUI {
 
         calcFrame = new JFrame();
         calcFrame.setTitle("Calculator");
-        calcFrame.setSize(250,300);
+        calcFrame.setSize(200,200);
         calcFrame.setLocation(200,200);
         calcFrame.setLayout(new BorderLayout());
         calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel keyboardPanel = new JPanel();
-        keyboardPanel.setLayout(new FlowLayout());
-        JTextArea display = new JTextArea(2,17);
+        keyboardPanel.setLayout(new GridLayout(1,2,5,5));
+
+        JPanel text = new JPanel(new GridLayout(1,1,5,5));
+        text.setSize(225,75);
+        JTextArea display = new JTextArea(1,25);
         display.setEditable(false);
         display.setFont(new Font ("Arial" , 14, 14));
+        display.setSize( 220,25);
+        JPanel but = new JPanel(new GridLayout(1,1,5,5));
         JButton clearAll = new JButton("AC");
         JScrollPane scrollPane = new JScrollPane(display);
+        clearAll.setSize(new Dimension(35,28));
 
+        JPanel buttonPanel = new JPanel(new GridLayout(4,4,5,5));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+        buttonPanel.add(new JButton("1"));
+
+        text.add(display);
+        but.add(clearAll);
         keyboardPanel.add(display);
         keyboardPanel.add(clearAll);
-        calcFrame.add(keyboardPanel);
+        calcFrame.add(keyboardPanel,BorderLayout.NORTH);
+        calcFrame.add(buttonPanel,BorderLayout.CENTER);
     }
 
 
